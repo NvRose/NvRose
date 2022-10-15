a849dacbe66fb22d99fd0e252724ab02d799d773
fenze <contact@fenze.dev>
2022-10-15 21:26:02 +0200

    initial commit

---
+++ .github/readme.md
@@ -0,0 +1,65 @@
+<h3 align=center> NvRose </h3>
+
+<p align=center>
+Neovim's lua config aiming to provide small but very fast IDE. <br>
+Cached and compiled plugins and if possible lazy loaded. <br>
+Minimalistic and aesthetic UI.
+</p>
+
+### Installation
+``` $ git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1```
+
+### Updating
+~/.config/nvim after installation is git repository, just ```git pull``` and fix merge conflicts. <br>
+
+### Plugins
+- Base:
+	- [wbthomason/packer.nvim](https://github.com/wbthomason/packer.nvim) - plugins manager.
+	- [NvRose/terminal.nvim](https://github.com/NvRose/terminal.nvim) - simple terminal emulator plugin.
+	- [lewis6991/impatient.nvim](https://github.com/lewis6991/impatient.nvim) - Speed up loading Lua modules in Neovim to improve startup time.
+	- [nvim-lua/plenary.nvim](https://github.com/plenary.nvim) - All the lua functions I don't want to write twice.
+
+- Coding:
+	- [neovim/nvim-lspconfig](https://github.com/neovim/nvim-lspconfig) - setting neovim's builtin lsp.
+	- [numToStr/Comment.nvim](https://github.com/numToStr/Comment.nvim) - vsc like commenting keybinds.
+	- [hrsh7th/nvim-cmp](https://github.com/hrsh7th/nvim-cmp) - completion engine.
+- UI:
+	- [catppuccin/nvim](https://github.com/catppuccin/nvim) - default colorscheme.
+	- [nvim-treesitter/nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter) - better highlighting.
+
+
+- Utils:
+	- [windwp/nvim-autopairs](https://github.com/windwp/nvim-autopairs) - autoclose brackets, strings...
+	- [nvim-telescope/telescope.nvim](https://github.com/nvim-telescope/telescope.nvim) - Finder/Filterer/Previewer/Picker.
+
+
+### Files
+- [init.lua](https://github.com/NvRose/NvRose/init.lua) - User plugins, keybinds, autocmds
+- [lua/base.lua](https://github.com/NvRose/NvRose/lua/base.lua) - Bootstrap, tweaks like disabled builtins/providers
+
+### Startup
+<table>
+	<tr><td> Resolve </td> <td> Load </td></tr>
+	<tr><td> 3.458ms </td> <td> 4.691ms </td></tr>
+</table>
+
+### Builtin tweaks
+- Disabled neovim builtin plugins
+- Disabled neovim builtin providers
+- Handy api bindings
+
+### Roadmap
+- WIP:
+	- Self update plugin
+- TODO:
+	- [ ] - Builtin tabs and statusbar plugins
+- FIXME:
+	- Fix hover lsp diagnostic window
+
+### 📜 License
+NvRose is released under MIT license, which grants the following permissions:
+- Commercial use
+- Distribution
+- Modification
+- Private use <br>
+For more details see [license](https://github.com/NvRose/terminal/license).
---
+++ init.lua
@@ -0,0 +1,280 @@
+require 'base'
+
+-- PLUGINS --
+
+use {
+	'NvRose/terminal.nvim',
+	config = function()
+		require('terminal').setup()
+	end
+}
+
+use {
+	'catppuccin/nvim',
+	as = 'catppuccin',
+	run = ':CatppuccinCompile',
+	config = function()
+		require('catppuccin').setup {
+			integrations = {
+				treesitter = true,
+				telescope  = true,
+				cmp        = true
+			},
+
+			custom_highlights = {
+				StatusLine = { bg = "NONE", fg = "#1e1e2e" },
+				StatusLineNC = { bg = "NONE", fg = "#1e1e2e" },
+			},
+
+			highlight_overrides = {
+				CursorLine = { bg = "NONE" },
+				NormalFloat = { bg = "NONE" },
+				Pmenu = { bg = "#1e1e2e" }
+			}
+		}
+
+		au("User", {
+			pattern = "PackerCompileDone",
+			callback = function()
+				vim.cmd "CatppuccinCompile"
+				vim.defer_fn(function()
+					colorscheme 'catppuccin'
+				end, 0)
+			end
+		})
+	end
+}
+
+use {
+	'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	config = function()
+		local actions = require 'telescope.actions'
+
+		require 'telescope'.setup {
+			defaults = {
+				set_env = { COLORTERM = 'truecolor' },
+				prompt_prefix = " ",
+				selection_caret = " ",
+				entry_prefix = " ",
+
+				file_ignore_patterns = {
+					'.git/',
+					'%.o',
+					'packer_compiled.lua'
+				},
+
+				mappings = {
+					i = {
+						["<esc>"] = actions.close,
+						["<cr>"] = actions.select_tab
+					}
+				},
+			},
+			pickers = {
+				find_files = {
+					theme = "dropdown",
+					preview_title = false,
+				},
+				live_grep = {
+					theme = "dropdown",
+					preview_title = false,
+				}
+			}
+		}
+	end
+}
+
+use {
+	'numToStr/Comment.nvim',
+	config = function()
+		require('Comment').setup {
+			padding = true,
+			sticky = true,
+			ignore = nil,
+			opleader = { block = '<c-/>' },
+			toggler = { block = '<c-/>' }
+		}
+	end
+}
+
+use {
+	'nvim-treesitter/nvim-treesitter',
+	run = ':TSUpdate',
+	setup = function()
+		vim.api.nvim_create_autocmd({
+			'BufRead', 'BufWinEnter', 'BufNewFile'
+		}, {
+			group = vim.api.nvim_create_augroup('LazyTreesitter', {}),
+			callback = function()
+				packer.loader('nvim-treesitter')
+				require'nvim-treesitter.configs'.setup {
+					auto_install = true,
+					highlight = {
+						enable = true,
+						additional_vim_regex_highlighting = false
+					}
+				}
+			end
+		})
+	end,
+	cmd = {
+		'TSInstall',
+		'TSBufEnable',
+		'TSBufDisable',
+		'TSEnable',
+		'TSDisable',
+		'TSModuleInfo'
+	}
+}
+
+use {
+	'hrsh7th/nvim-cmp',
+	requires = {
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-buffer',
+		'hrsh7th/cmp-path',
+		'L3MON4D3/LuaSnip',
+		'saadparwaiz1/cmp_luasnip'
+
+	},
+	config = function()
+		local has_words_before = function()
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+		end
+
+		local luasnip = require 'luasnip'
+		local cmp = require 'cmp'
+		cmp.setup {
+			window = {
+				completion = {
+					border = "rounded"
+				},
+
+				documentation = {
+					border = "rounded"
+				}
+			},
+
+			mapping = {
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item {
+							behavior = cmp.SelectBehavior.Select
+						}
+					elseif luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item {
+							behavior = cmp.SelectBehavior.Select
+						}
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				['<c-space>'] = cmp.mapping.complete(),
+
+				['<CR>'] = cmp.mapping.confirm {
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true
+				}
+			},
+
+			sources = cmp.config.sources {
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' },
+				{ name = 'buffer' },
+				{ name = 'path' },
+			},
+
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end
+			}
+		}
+	end
+}
+
+use {
+	'windwp/nvim-autopairs',
+	config = function()
+		require 'nvim-autopairs'.setup()
+	end
+}
+
+-- USER INTERFACE --
+
+colorscheme 'catppuccin'
+
+lsp_icons {
+	['Error'] = '',
+	['Warn']  = '',
+	['Info']  = '',
+	['Hint']  = ''
+}
+
+diagnostic.config {
+	virtual_text     = false,
+	signs            = true,
+	underline        = true,
+	update_in_insert = false
+}
+
+-- NEOVIM OPTIONS --
+
+g.mapleader   = ' '
+opt.clipboard = 'unnamedplus'
+opt.wrap      = false
+opt.ls        = 0
+opt.ru	      = false
+opt.smd	      = false
+opt.cul       = true
+opt.nu        = true
+opt.sb        = true
+opt.spr       = true
+opt.scl       = 'number'
+opt.ch        = 0
+
+-- KEY MAPPINGS --
+
+map('n', ']d', vim.diagnostic.goto_next)
+map('n', '[d', vim.diagnostic.goto_prev)
+map('n', 'gd', vim.lsp.buf.definition)
+map('n', 'gf', function()
+	return vim.lsp.buf.code_action {
+		apply = true
+	}
+end)
+
+local ts_builtin = require('telescope.builtin')
+map('n', '<c-n>', ts_builtin.find_files)
+map('n', '<c-f>', ts_builtin.live_grep)
+map('n', ';',     ts_builtin.command_history)
+
+-- AUTOMATIC COMMANDS --
+
+au('BufWrite', {
+	pattern = '*', command = [[ sil! :%s#\($\n\s*\)\+\%$## | :%s/\s\+$//e ]]
+})
+
+-- LSP --
+
+lsp.clangd.setup { -- example, you can take configs from mason-nvim
+	capabilities = require('cmp_nvim_lsp').default_capabilities(),
+	restartAfterCrash = true,
+	detectExtensionConflicts = true,
+	semanticHighlighting = true,
+	serverCompletionRanking = true
+}
